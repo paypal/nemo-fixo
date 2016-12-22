bd8b6a62120c0c15a3d2b4f4565da9aab46d64ac
Herryanto Siatono <hsiatono@paypal.com>
2016-12-22 09:35:05 +0800

    Add test cases to load object with callback and promise

--- test/spec/basic.js
+++ test/spec/basic.js
@@ -18,6 +18,22 @@ describe('@basic: Init and set options', function () {
         done();
     });
 
+    it('should load object with a callback', function (done) {
+        nemo.fixo.load('profile', function (err, profile) {
+            if (err) {
+                return done(err);
+            }
+            assert.equal(profile.name, 'Walter Mitty');
+            done();
+        });
+    });
+
+    it('should load object with a promise', function (done) {
+        nemo.fixo.load('profile').then(function (profile) {
+            assert.equal(profile.name, 'Walter Mitty');
+        }).then(done, done);
+    });
+
     after(function (done) {
         if (nemo.driver) {
             nemo.driver.quit().then(done);
